3e87bbba1591971b08eaf2d89cd2df8ecc4f81d8
Eduardo Moranchel <emoranchel@gmail.com>
2015-04-16 15:44:09 -0500

    LoS Works!

--- src/main/java/net/edzero/fow/impl/BresenhamFogOfWar.java
+++ src/main/java/net/edzero/fow/impl/BresenhamFogOfWar.java
@@ -1,11 +1,13 @@
 package net.edzero.fow.impl;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import net.edzero.fow.FogOfWar;
 import net.edzero.fow.util.Geometry;
 
 public class BresenhamFogOfWar implements FogOfWar {
 
   private byte[][] data;
+  private boolean[][] obstacles;
   private int width;
   private int height;
 
@@ -13,6 +15,7 @@ public class BresenhamFogOfWar implements FogOfWar {
   public void initialize(int width, int height) {
     this.width = width;
     this.height = height;
+    obstacles = new boolean[width][height];
     data = new byte[width][height];
     for (int x = 0; x < width; x++) {
       for (int y = 0; y < height; y++) {
@@ -23,14 +26,20 @@ public class BresenhamFogOfWar implements FogOfWar {
 
   @Override
   public void addObstacle(int x, int y) {
+    obstacles[x][y] = true;
   }
 
   @Override
   public void addPointOfView(int cx, int cy, int distance) {
     Geometry.circle(cx, cy, distance, (int destinationX, int destinationY) -> {
-      //mark(destinationX, destinationY);
+      AtomicBoolean obstructed = new AtomicBoolean(false);
       Geometry.line(cx, cy, destinationX, destinationY, (int lineX, int lineY) -> {
-        reveal(lineX, lineY);
+        if (!obstructed.get()) {
+          reveal(lineX, lineY);
+          if (isObstacle(lineX, lineY)) {
+            obstructed.set(true);
+          }
+        }
       });
     });
   }
@@ -44,6 +53,13 @@ public class BresenhamFogOfWar implements FogOfWar {
     if (x >= 0 && x < width && y >= 0 && y < height) {
       data[x][y] = VISIBLE;
     }
+  }
+
+  private boolean isObstacle(int x, int y) {
+    if (x >= 0 && x < width && y >= 0 && y < height) {
+      return obstacles[x][y];
+    }
+    return true;
   }
 
 }
